897e802e79db2e01ce2baa84a793438c88763722
sophiaaaxa <sophialeabo@gmail.com>
2024-07-24 15:04:17 -0400

    fixed image.ppm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+
+    let image_width: i32 = 256;
+    let image_height: i32 = 356;
+
+    println!("P3");
+    println!("{} {}", image_width, image_height);
+    println!("255");
+
+    for j in 0..image_height{
+            for i in 0..image_width{
+                let r = i as f64 / (image_width - 1) as f64;
+                let g = j as f64 / (image_height - 1) as f64;
+                let b = 0.0;
+
+                let ir = (255.999 * r) as i32;
+                let ig = (255.999 * g) as i32;
+                let ib = (255.999 * b) as i32;
+
+                println!("{} {} {}", ir, ig, ib);
+
+            }
+    }
+
+
 }
